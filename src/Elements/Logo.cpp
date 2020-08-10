0d59e9ae8cce0962ee1ea3663ce1ec0db008c70c
Filip Budiša <filip@circuitmess.com>
2020-08-10 17:45:32 +0200

    Remove logo debug output

--- src/Elements/Logo.cpp
+++ src/Elements/Logo.cpp
@@ -11,7 +11,6 @@ Logo::Logo(Sprite* canvas) : canvas(canvas), x((canvas->width() - width) / 2), c
 void Logo::update(uint micros){
 	f += (float) micros / 1000000.0f;
 	if(f > 2 * M_PI) f -= 2 * M_PI;
-	Serial.printf("%.3f\n", f);
 
 	currentY = y + pow(cos(f * 1000.0f / speed), 2) * -amplitude;
 }
